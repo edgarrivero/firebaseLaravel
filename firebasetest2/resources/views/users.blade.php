797c0f3f82a326ae8c9c34d49c558be76f729a59
Edgar Rivero <riveroedgar.jm@gmail.com>
2022-10-01 12:48:04 -0500

    Style general edit

--- firebasetest2/resources/views/users.blade.php
+++ firebasetest2/resources/views/users.blade.php
@@ -5,65 +5,68 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="/resources/css/app.css" rel="stylesheet">
 
     <title>Hello, world!</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.15.3/xlsx.full.min.js"></script>
 </head>
 </head>
-<body class="container">
-<div class="row p-4">
-    <div class="col">
-        <form id="formUser">
-            <div class="mb-3">
-                <label for="name" class="form-label">Nombre</label>
-                <input type="text" class="form-control" id="name">
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="button" class="btn btn-primary" id="submitUser">Submit</button>
-        </form>
-        <h4>Subir Excel</h4>
-        <div class="panel panel-primary">
-            <div class="panel-heading">Converso de EXCEL a JSON</div>
-            <div class="panel-body">
-                <!-- Input type file to upload excel file -->
-                <div class="input-group">
-                    <input type="file" class="form-control" id="fileUpload" accept=".xls,.xlsx"/><br/>
-                    <button type="button" id="uploadExcel" class="btn btn-primary">Guardar</button>
+<body class="bg-secondary bg-opacity-25">
+    <div class="container">
+        <div class="row p-4">
+            <div class="col   m-3 ">
+                <div class="bg-white p-4 rounded-3 shadow-sm">
+                    <h4>Guardar Registro</h4>
+                    <form id="formUser">
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nombre</label>
+                            <input type="text" class="form-control" id="name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                        </div>
+
+                        <button type="button" class="btn btn-primary" id="submitUser">Guardar</button>
+                    </form>
+                    <hr>
+                    <h4>Subir Excel</h4>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Guardar excel en base de datos</div>
+                        <div class="panel-body">
+                            <!-- Input type file to upload excel file -->
+                            <div class="input-group">
+                                <input type="file" class="form-control" id="fileUpload" accept=".xls,.xlsx"/><br/>
+                                <button type="button" id="uploadExcel" class="btn btn-primary">Guardar</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+            </div>
+            <div class="col bg-white p-4 m-3 rounded-3 shadow-sm">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Email</th>
+                    </tr>
+                    </thead>
+                    <tbody id="tableBody">
+
+
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
-    <div class="col">
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">nombre</th>
-                <th scope="col">email</th>
-            </tr>
-            </thead>
-            <tbody id="tableBody">
 
 
-            </tbody>
-        </table>
-    </div>
-</div>
-
-
-<script type="module">
+    <script type="module">
 
 
     $(document).ready(function () {
@@ -88,7 +91,6 @@
                 data: JSON.stringify(model),
                 success: function (r) {
                     console.log(r);
-
                 },
                 error: function (xhr, status, error) {
                     if (xhr.responseJSON !== undefined && xhr.responseJSON.errorBag !== undefined) {
@@ -118,25 +120,26 @@
                 var objectToInspect;
                 var result = [];
 
-                for(objectToInspect = r; objectToInspect !== null;
-                    objectToInspect = Object.getPrototypeOf(objectToInspect)) {
+                for (objectToInspect = r; objectToInspect !== null;
+                     objectToInspect = Object.getPrototypeOf(objectToInspect)) {
                     result = result.concat(
                         Object.getOwnPropertyNames(objectToInspect)
                     );
                 }
 
-                result.forEach(function (e,i) {
-                     if( e.charAt(0) === "-" ){
-                             let records = `<tr>
+                result.forEach(function (e, i) {
+                    i++;
+                    if (e.charAt(0) === "-") {
+                        let records = `<tr>
                                                   <th>${i} </th>
                                                   <th>${r[e].name} </th>
                                                   <td>${r[e].email}</td>
                                               </tr>`;
-                              $("#tableBody").append(records);
-                     }
+                        $("#tableBody").append(records);
+                    }
 
 
-                 })
+                })
             },
             error: function (xhr, status, error) {
                 if (xhr.responseJSON !== undefined && xhr.responseJSON.errorBag !== undefined) {
@@ -152,7 +155,7 @@
 
 </script>
 
-<script>
+    <script>
     var selectedFile;
 
     var obj = null;
@@ -225,18 +228,18 @@
 
 </script>
 
-<!-- Optional JavaScript; choose one of the two! -->
+    <!-- Optional JavaScript; choose one of the two! -->
 
-<!-- Option 1: Bootstrap Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"></script>
 
 
-<!-- Option 2: Separate Popper and Bootstrap JS -->
-<!--
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
--->
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
 </body>
 </html>
